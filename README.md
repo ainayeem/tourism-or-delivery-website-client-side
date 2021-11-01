1857bc7ba5b8cc439f63f05243bbfa78f43112b3
ainayeem <ain.nayeem1999@gmail.com>
2021-11-01 22:54:34 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Travel Bizz
+
+### A simple website Where a user can book a Tour place to enjoy his vacation.
+
+# Features
+
+- user can see his Booking Status.
+- User can Update Services
+- Can Delete booking by user.
+- Latest Tour News.
+- An Admin can update Booking status.
+
+## live Site Link: https://travel-bizz.web.app/
---
+++ src/Context/Firebase/firebase.confiq.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBC8yZ4q8iNsINXbQ4pTp0bPUdrDwwNeFY",
+  authDomain: "travelblizz-3bbd7.firebaseapp.com",
+  projectId: "travelblizz-3bbd7",
+  storageBucket: "travelblizz-3bbd7.appspot.com",
+  messagingSenderId: "1073847013496",
+  appId: "1:1073847013496:web:d1f266d3b5934743ca5d8d",
+};
+
+export default firebaseConfig;
